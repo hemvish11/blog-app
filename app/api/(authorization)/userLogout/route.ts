28a8c3ffcf6888d685686bcf1088d564a9518f2f
Hemant Vishwakarma <hemvish11@gmail.com>
2024-08-08 12:22:35 +0530

    logout route removed

--- app/api/(authorization)/userLogout/route.ts
+++
@@ -1,6 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import { NextResponse } from "next/server";
-
-export const POST = async (req: NextResponse) => {
-  return new NextResponse(JSON.stringify({ message: "Logged out" }));
-};
